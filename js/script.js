bbe1d18d8a7c98811801c6e106003b67bea500d1
israel <israel.pilps@gmail.com>
2018-10-30 15:56:15 -0300

    novo reposition

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var app4 = novo Vue ({ 
+    el: '# app-4' , 
+    data: { 
+      todos: [ 
+        { text : 'Aprenda JavaScript' }, 
+        { text : 'Aprenda Vue' }, 
+        { texto : 'Construa algo incrível' } 
+      ] 
+    } 
+  })
