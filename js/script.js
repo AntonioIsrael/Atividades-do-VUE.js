1bc31f7ef9c595c0449cbc7d0173b80641ca3f7e
israel <israel.pilps@gmail.com>
2018-11-01 15:35:28 -0300

    Novas coisas

--- js/script.js
+++ js/script.js
@@ -1,10 +1,6 @@
-var app4 = novo Vue ({ 
-    el: '# app-4' , 
-    data: { 
-      todos: [ 
-        { text : 'Aprenda JavaScript' }, 
-        { text : 'Aprenda Vue' }, 
-        { texto : 'Construa algo incrível' } 
-      ] 
-    } 
+var app = new Vue({
+    el: '#app',
+    data: {
+      msg: 'aaaaaaa'
+    }
   })
